3821ad6e121bd675987db56ea8f3717af30c35ae
Wellington <hanzo.hasashi.pmk@gmail.com>
2021-12-19 04:26:11 -0300

    Style almost finished; Finish functionality

--- index.js
+++ index.js
@@ -23,38 +23,82 @@ function chooseForHouse() {
   }
 }
 
-window.onload = () => {
+var mainContainer = document.querySelector('.main-container') || document.createElement('div');
+mainContainer.className = 'main-container';
 
+var resultContainer = document.createElement('div');
+resultContainer.className = 'result';
+resultContainer.innerHTML = `
+  <div id="your-choice"></div>
+  <div id="house-choice"></div>
+  <div id="result"></div>
+`;
+
+window.onload = () => {
   // temp
   document.querySelectorAll('.play-button').forEach(button => {
     button.addEventListener('click', (ev) => {
       const buttonValue = ev.currentTarget.value;
       const houseChoice = chooseForHouse();
 
-      console.log(`house chose: ${houseChoice}`);
+      const main = document.querySelector('main');
+      main.removeChild(mainContainer);
+      main.appendChild(resultContainer);
+
+      document.getElementById('your-choice').innerHTML = `
+        <button class="play-button ${buttonValue}" value="${buttonValue}">
+          <div class="image">
+            <img src="./images/icon-${buttonValue}.svg">
+          </div>
+        </button>
+        <p>You picked</p>
+      `;
+
+      document.getElementById('house-choice').innerHTML = `
+        <button class="play-button ${houseChoice}" value="${houseChoice}">
+          <div class="image">
+            <img src="./images/icon-${houseChoice}.svg">
+          </div>
+        </button>
+        <p>The house picked</p>
+      `;
+
+      let message = '';
 
       if (houseChoice == buttonValue) {
-        console.log('draw, no one won');
+        message = 'draw';
       } else if (winningRelationships[houseChoice] != buttonValue) {
-        console.log('you won');
+        message = 'you won';
         playerScore++;
         document.getElementById('player-score').innerText = playerScore;
       } else {
-        console.log('you lost');
+        message = 'you lose';
       }
+
+      document.getElementById('result').innerHTML = `
+        <h2>${message}</h2>
+        <button id="play-again-button">Play again</button>
+      `;
+
+      document.getElementById('play-again-button')
+        .addEventListener('click', () => {
+          main.removeChild(resultContainer);
+          main.appendChild(mainContainer);
+        });
+
     });
   });
 
   // temp rule on/off
-  document.getElementById('show-images-button').addEventListener('click', e => {
-    const rulesContainer = e.target.nextElementSibling;
-    console.log(rulesContainer);
-    if (e.target.dataset.showing === 'no') {
-      e.target.dataset.showing = 'true';
-      rulesContainer.style.display = 'block';
-    } else {
-      e.target.dataset.showing = 'no';
-      rulesContainer.style.display = 'none';
-    }
-  })
+  document.getElementById('show-rules-button').addEventListener('click', e => {
+    const rulesContainer = e.currentTarget.nextElementSibling;
+    rulesContainer.style.display = 'block';
+    rulesContainer.style.opacity = '1';
+  });
+
+  document.getElementById('close-rules-button').addEventListener('click', e => {
+    const rulesContainer = e.currentTarget.parentElement;
+    rulesContainer.style.opacity = '0';
+    setTimeout(() => rulesContainer.style.display = 'none', 100)
+  });
 }
